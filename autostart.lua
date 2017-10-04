1d77012d68267c018be73206859c791bd4bc35aa
Tommy Mattsson <tommy.mattsson@teliacompany.com>
2017-10-04 17:03:02 +0200

    Applications will now only start if they are not started (doesn't work for all programs...)

--- autostart.lua
+++ autostart.lua
@@ -2,10 +2,20 @@ local awful = require("awful")
 
 local var = {}
 
+local function run_once(app)
+   local fpid = io.popen("pgrep " .. app)
+   local pid = fpid:read("*n")
+   fpid:close()
+
+   if pid == nil then
+      awful.spawn.with_shell(app)
+   end
+end
+
 local function applications(apps)
    awful.spawn.easy_async("numlockx on")
    for _,app in ipairs(apps) do
-      awful.spawn.with_shell(app)
+      run_once(app)
    end
 end
 
