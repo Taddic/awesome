b730cc1ef9eea2bd42806bc5f7bea357bb402494
Taddic <tommy.mattsson@gmail.com>
2017-10-10 12:14:26 +0200

    Moved some code around

--- lib/rules.lua
+++ lib/rules.lua
@@ -3,28 +3,41 @@
 --------------------------------------------------------------------------------
 local awful       = require("awful")
 local beautiful   = require("beautiful")
+local screen_lib  = require("awful.screen")
+
+--------------------------------------------------------------------------------
+-- User defined libraries                                                     --
+--------------------------------------------------------------------------------
 local keybindings = require("lib.keybindings")
 
+--------------------------------------------------------------------------------
+-- Local variables/functions                                                  --
+--------------------------------------------------------------------------------
+local function mkRule(new_rule,new_prop)
+   return { rule = new_rule, properties = new_prop }
+end
+
+local function mkAnyRule(new_rule, new_prop)
+   return {rule_any = new_rule, properties = new_prop }
+end
+
+local function insertRule(rule)
+   table.insert(awful.rules.rules, rule)
+end
+
+--------------------------------------------------------------------------------
+-- Exported variables/functions                                               --
+--------------------------------------------------------------------------------
 local function setup(rules)
-   local function mkRule(new_rule,new_prop)
-      return { rule = new_rule, properties = new_prop }
-   end
-
-   local function mkAnyRule(new_rule, new_prop)
-      return {rule_any = new_rule, properties = new_prop }
-   end
-
-   local function insertRule(rule)
-      table.insert(awful.rules.rules, rule)
-   end
-   insertRule(mkRule({},{ border_width = beautiful.border_width,
-						border_color = beautiful.border_normal,
-						focus = awful.client.focus.filter,
-						raise = true,
-						keys = keybindings.clientkeys,
-						buttons = keybindings.clientbuttons,
-						screen = awful.screen.preferred,
-						placement = awful.placement.no_overlap+awful.placement.no_offscreen}))
+   insertRule(mkRule({},{
+		    border_width = beautiful.border_width,
+		    border_color = beautiful.border_normal,
+		    focus = awful.client.focus.filter,
+		    raise = true,
+		    keys = keybindings.clientkeys,
+		    buttons = keybindings.clientbuttons,
+		    screen = awful.screen.preferred,
+		    placement = awful.placement.no_overlap+awful.placement.no_offscreen}))
 
    insertRule(mkAnyRule({instance = {"DTA",        -- Firefox addon DownThemAll.
 				     "copyq",      -- Includes session name in class.
