9b3646232fed497d389ba5736b925c70a87212f9
Tommy Mattsson <tommy.mattsson@teliacompany.com>
2017-10-10 09:56:19 +0200

    Moved arguments to variables for easier adding/removing of widgets, rules and autostart apps

--- rc.lua
+++ rc.lua
@@ -24,26 +24,34 @@ mynetworklauncher = awful.widget.launcher(
    {image = beautiful.awesome_icon,
     menu = awful.menu({items = netmgr.generate_network_menu()})})
 
+
+
+local widgets = {
+   spotify_widget,
+   brightness_widget,
+   volume_widget,
+   battery_widget,
+   mynetworklauncher,
+}
+
+local app_rules = {
+   {{class = "Chromium-browser"}, {screen = 2, tag = "2"}},
+   {{class = "discord"},          {screen = 3, tag = "2"}},
+   {{class = "Firefox"},          {screen = 2, tag = "3"}},
+   {{class = "Spotify"},          {screen = 1, tag = "1"}},
+   {{class = "Thunderbird"},      {screen = 3, tag = "3"}},
+}
+
+local app_autostart = {
+   "chromium-browser",
+   "discord",
+   "spotify",
+   "thunderbird",
+}
 --------------------------------------------------------------------------------
 -- Configuration                                                              --
 --------------------------------------------------------------------------------
-standard_cfg.setup({spotify_widget,
-		    brightness_widget,
-		    volume_widget,
-		    battery_widget,
-		    mynetworklauncher})
-
+standard_cfg.setup(widgets, keybindings.modkey)
 keybindings.setup()
-
-rules.setup({
-      {{class = "Chromium-browser"}, {screen = 2, tag = "2"}},
-      {{class = "discord"},          {screen = 3, tag = "2"}},
-      {{class = "Firefox"},          {screen = 2, tag = "3"}},
-      {{class = "Spotify"},          {screen = 1, tag = "1"}},
-      {{class = "Thunderbird"},      {screen = 3, tag = "3"}}})
-
-
-autostart.setup({"chromium-browser",
-		 "discord",
-		 "spotify",
-		 "thunderbird"})
+rules.setup(app_rules)
+autostart.setup(app_autostart)
